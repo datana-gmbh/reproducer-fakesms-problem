318acd5d7b3cb126e0957c38cc98c751f12631ed
Oskar Stark <oskarstark@googlemail.com>
2024-02-02 16:40:42 +0100

    -

---
+++ src/Command/TestCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\TexterInterface;
+
+#[AsCommand(
+    name: 'app:test',
+    description: 'Add a short description for your command',
+)]
+class TestCommand extends Command
+{
+    public function __construct(
+        private TexterInterface $texter,
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->texter->send(new SmsMessage(
+            '+33612345678',
+            'Hello from Symfony Notifier!'
+        ));
+
+        return Command::SUCCESS;
+    }
+}
